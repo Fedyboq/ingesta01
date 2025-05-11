d447209eac774a1ba362d1816aab9d1ee6074da5
Fedyboq <166071546+Fedyboq@users.noreply.github.com>
2025-05-11 10:41:39 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -6,8 +6,8 @@ ficheroUpload2 = "categorias.csv"
 nombreBucket = "prdocutos-cloud-pp"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
-response1 = s3.upload_file(ficheroUpload1, nombreBucket, ficheroUpload1)
-response2 = s3.upload_file(ficheroUpload2, nombreBucket, ficheroUpload2)
+response = s3.upload_file(ficheroUpload, nombreBucket, "productos/productos.csv")
+response1 = s3.upload_file(ficheroUpload1, nombreBucket, "tiene/tiene.csv")
+response2 = s3.upload_file(ficheroUpload2, nombreBucket, "categorias/categorias.csv")
 
 print("Ingesta completada")
